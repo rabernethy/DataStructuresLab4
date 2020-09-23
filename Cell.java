d66a5ca675e20ee85424d8a5d225ae1cf370e528
Russell <rdaswimmer@gmail.com>
2020-09-23 15:56:33 -0400

    cleaned up code.

--- Cell.java
+++ Cell.java
@@ -18,22 +18,21 @@ public class Cell extends JPanel{
 	public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLACK);
-        //g.drawString(row +"," + col, 10, 10);
         
         int y = this.getHeight();
         int x = this.getWidth();
         g.setColor(Color.BLACK);
         if(northWall) {
-
         	g.drawLine(0,0,x, 0);
-        }if(southWall) {
+		}
+		if(southWall) {
         	g.drawLine(0,y,x, y);
-        }if(eastWall) {
-        	g.drawLine(x,0,x, y);
-        	
-        }if(westWall) {
-        	g.drawLine(0,0,0, y);
-        	
+		}
+		if(eastWall) {
+        	g.drawLine(x,0,x, y);	
+		}
+		if(westWall) {
+        	g.drawLine(0,0,0, y);	
         }
         
         
